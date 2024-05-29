a41e0d48db8f0a93d63e06c796630d38117e3440
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-05-29 16:18:07 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+//******************************************************************************
+// File name:   main.cpp
+// Author:      CS Faculty
+// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
+// Class:       CS150-xx (Replace xx with your section number)
+// Assignment:  Hello World
+// Purpose:     Display the message Hello World to the screen
+// Hours:       0.25
+//******************************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  cout << "Hello World" << endl;
+  
+  return EXIT_SUCCESS;
+}
